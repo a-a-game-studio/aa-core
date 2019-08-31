c6219c0cd8250e3aefd4313a28732ca3b47d9051
angel <angelrinascita@gmail.com>
2019-08-31 12:55:35 +0400

    Изменил  MainRequest на Core

--- src/System/Core.ts
+++ src/System/Core.ts
@@ -8,115 +8,84 @@ import { ResponseSys } from './ResponseSys';
 
 import { Request } from 'express';
 
-export default interface MainRequest extends Components. {
-    headers: { [key: string]: any };
-    body: any;
-    method: string;
+/**
+ * Конфигурация
+ */
+export interface ConfI extends Components.BaseConfI {
+    env:string;
+    // ================================
+    mysql: { // Конфиг для MySql
+        client: string, // mysql
+        connection: { // Параметры соединения
+            host: string; // "127.0.0.1"
+            user: string; // Пользователь
+            password: string; // Пароль
+            database: string; // Имя базы данных
+        },
+        pool: { min: number, max: number }, // Количество соединений (3,7)
+        migrations: {
+            tableName: string; // "knex_migrations",
+            directory: string; // "./src/Infrastructure/SQL/Migrations"
+        },
+        acquireConnectionTimeout: number; // таймоут 60000
+    };
+    // ================================
+    redis?: { // Конфиг для редиса
+        url: string; // "redis://127.0.0.1:6379"
+    };
+    // ================================
+    rabbit?: { // Rabit очереди
+        connection: string;
+    };
 
+    S3?: { // S3 хранилище
+        endpoint: string;
+        bucket: string;
+        access: string;
+        secret: string;
+    },
+
+}
+
+export interface CoreI extends Components.BaseCoreI {
+    req?:Request;
     sys: {
-        apikey: string,
-        bAuth: boolean, /* флаг авторизации */
-        errorSys: ErrorSys,
-        userSys: UserSys,
+        errorSys: Components.ErrorSys;
+        userSys?: UserSys,
         responseSys: ResponseSys
     };
-    conf: { // Конфигурация
-        // ================================
-        mysql: { // Конфиг для MySql
-            client: string, // mysql
-            connection: { // Параметры соединения
-                host: string; // "127.0.0.1"
-                user: string; // Пользователь
-                password: string; // Пароль
-                database: string; // Имя базы данных
-            },
-            pool: { min: number, max: number }, // Количество соединений
-            migrations: {
-                tableName: string; // "knex_migrations",
-                directory: string; // "./src/Infrastructure/SQL/Migrations"
-            },
-            acquireConnectionTimeout: number; // таймоут 60000
-        };
-        // ================================
-        pgsql: { // Конфиг для Postgress
-            dialect: string; // "postgres",
-            username: string; // Имя пользователя,
-            password: string; // Пароль
-            host: string; // "127.0.0.1",
-            port: number; // 5432,
-            database: string; // Имя базы данных
-            dialectOptions: {
-                supportBigNumbers: true;
-                decimalNumbers: true;
-            }
-        };
-        // ================================
-        redis: { // Конфиг для редиса
-            url: string; // "redis://127.0.0.1:6379"
-        };
-        // ================================
-        common: { // Общее
-            env: string; // Тип окружения
-            oldCoreURL: string; // URL адрес основного сайта
-        };
-
-        rabbit: {
-            connection: string;
-        };
-
-        S3: {
-            endpoint: string;
-            bucket: string;
-            baseUrl: string;
-            access: string;
-            secret: string;
-        },
-
-        SeoConfig?: SeoConfigI
-
-    },
-    infrastructure:{
-        mysql:any;
-        redis:any;
-        rabbit: any;
+    conf:ConfI
+    infrastructure?:{
+        mysql?:any;
+        redis?:any;
+        rabbit?: any;
     }
 }
 
-const Req: any = {
-    headers: null,
-    common: { // Общее
-        env: 'dev', // Тип окружения
-        oldCoreURL: null, // URL адрес основного сайта
-    },
-    sys: {
-        apikey: '',
-        bAuth: false, /* флаг авторизации */
-
-        errorSys: null,
-        userSys: null,
-        responseSys: null
-    },
-    conf:null,
-    infrastructure:{
-        mysql:null,
-        redis:null
-    }
-};
-
-
-export const devReq = <MainRequest> Req;
 
 /**
  * Инициализация MainRequest для консольных запросов
  */
-export function initMainRequest(conf: any): MainRequest {
+export function initMainRequest(conf: ConfI): CoreI {
 
-    let mainRequest: MainRequest;
+    // Инициализация базового ядра
+    let baseCoreObj: Components.BaseCoreI = Components.initBaseCore(conf);
 
-    mainRequest = devReq;
-    mainRequest.conf = conf;
+    let coreObj:CoreI = {
+        env: baseCoreObj.env, // Тип окружения
+        apikey: baseCoreObj.apikey, // API ключ
+        sys: { // Системные сервисы
+            errorSys: baseCoreObj.sys.errorSys, // Глобальная система ошибок
+            userSys: null, // Система пользователя
+            responseSys: null
+        },
+        conf: conf, // Конфигурация
+        infrastructure:{ // Инфраструктурные компоненты
+            mysql:null, // База данных MySQL
+            redis:null, // Редис
+            rabbit:null, // Ребит
+        }
+    };
 
-    mainRequest.sys.errorSys = new ErrorSys(mainRequest);
-
-    return mainRequest;
+    return coreObj;
 }
